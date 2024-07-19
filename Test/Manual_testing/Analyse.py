4afac8f96968216e895bc5c2e43e5315893bad5a
RoPerrier <romain.perrier13@gmail.com>
2024-07-19 11:31:53 +0100

    Add time curve

--- Test/Manual_testing/Analyse.py
+++ Test/Manual_testing/Analyse.py
@@ -10,23 +10,25 @@ metrics = data['Metric'].unique()
 
 models = ['Rubric', 'ACJ', 'ACJ variant', 'CTJ']
 
-# Create a new figure with a specific size to add clarity
-plt.figure(figsize=(10, 6))
-
-# Loop through each model
-for model in models:
-    # Filter the data to include only rows where 'Metric' is 'accuracy' and select only the columns for 'Judge' and the current model
-    model_data = data[data['Metric'] == 'accuracy'][['Judge', model]]
+for metric in ['accuracy','time']:
     
-    # Plot the accuracy for the current model against the judges
-    plt.plot(model_data['Judge'], model_data[model], marker='o', label=model)
-
-# Display the plot
-plt.title('Accuracy depending on the judges')
-plt.xlabel('Judges')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.xticks(rotation=45)
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+    # Create a new figure with a specific size to add clarity
+    plt.figure(figsize=(10, 6))
+    
+    # Loop through each model
+    for model in models:
+        # Filter the data to include only rows where 'Metric' is 'accuracy' and select only the columns for 'Judge' and the current model
+        model_data = data[data['Metric'] == metric][['Judge', model]]
+        
+        # Plot the accuracy for the current model against the judges
+        plt.plot(model_data['Judge'], model_data[model], marker='o', label=model)
+    
+    # Display the plot
+    plt.title(f'{metric} depending on the judges')
+    plt.xlabel('Judges')
+    plt.ylabel(f'{metric}')
+    plt.legend()
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
